8557d8cf41eddaced9402be73e73267f44d367a0
gsytec <gsytec@gmail.com>
2016-01-17 13:15:49 +0530

    error done!!!

--- src/com/gyan/helloworld/HelloWorld.java
+++ src/com/gyan/helloworld/HelloWorld.java
@@ -2,11 +2,8 @@ package com.gyan.helloworld;
 
 public class HelloWorld {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		System.out.println("Welcome to Ant");
+		System.out.println("Welcome to Ant")
 
 	}
 
